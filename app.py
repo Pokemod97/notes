9bdb7c2cbc94c61cd4d9c5575b51becc74fd72e6
Caden Kline <cadenkline9@gmail.com>
2019-03-14 20:32:49 -0400

    Added temporary notes functionality

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import *
 app = Flask(__name__)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def index():
+    return render_template('index.html', notes=session['notes'])
 
 
-@app.route('/post')
+@app.route('/post', methods=['POST', 'GET'])
 def post():
+    if request.method == 'POST':
+        if session.get('notes') is None:
+            session['notes'] = []
+        session['notes'].append((request.form['title'], request.form['note']))
+        session.modified = True
+        return redirect(url_for('index'))
     return render_template('post.html')
-
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    print(request.form.get('title'))
-    return 'hi'
 
 
 if __name__ == '__main__':
